704be872a703b2d166bdc908fbd57830bb5643c6
diacomltd <diacomltd@mail.ru>
2017-12-27 01:03:14 +0300

    tests fix

--- tests/PHPConst2JSTest.php
+++ tests/PHPConst2JSTest.php
@@ -1,11 +1,29 @@
-<?php 
+<?php
 
-class PHPConst2JSTest extends PHPUnit_Framework_TestCase{
-	
-  public function testIsThereAnySyntaxError(){
-	$var = new Sunmant\PHPConst2JS\PHPConst2JS;
-	$this->assertTrue(is_object($var));
-	unset($var);
-  }
-  
+use Sunmant\PHPConst2JS\PHPConst2JS;
+
+class ClassWithConstants
+{
+    const CONSTANT1 = 1;
+}
+
+class PHPConst2JSTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsThereAnySyntaxError()
+    {
+        $var = new PHPConst2JS;
+        $this->assertTrue(is_object($var));
+        unset($var);
+    }
+
+    public function testAddConstAndGetJSON()
+    {
+        $var = new PHPConst2JS();
+        $var->addClassConstants(ClassWithConstants::class);
+
+        $arr['ClassWithConstants::CONSTANT1'] = ClassWithConstants::CONSTANT1;
+        $this->assertSame($var->getJSON(), json_encode($arr));
+        unset($var);
+    }
+
 }
